1ba6d46a60525e550cf8ba0e2545fb0fac87a439
Martin Dimitrov <dimitrov.martin.v@gmail.com>
2020-04-29 13:04:02 +0300

    Additions to the endpoint

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 from flask import Flask, request, jsonify
 import boto3
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/api/create-customer', methods=['POST'])
+@app.route('/api/customers', methods=['POST'])
 def create_customer():
     '''
-    Format of json request is
+    Request body looks like this
     {
-        'name': 'John Doe',
+        'name': 'John Doe',dock
         'username': 'johndoe'
     }
     '''
-    data = request.json
-    name = data['name']
-    username = data['username']
-
-    # set up DynamoDB connection
-    dynamodb = boto3.resource('dynamodb', region_name='eu-central-1', aws_access_key_id='AKIAUBFFK6SOG24HUIFW', aws_secret_access_key='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')
+    # Validation
+    if not 'name' in request.json or not 'username' in request.json:
+        return 'Incorrect input data. Provide both name and username'
+        
+    # set up DynamoDB connection and get the table
+    dynamodb = boto3.resource('dynamodb', region_name='eu-central-1') # aws_access_key_id='xxx', aws_secret_access_key='xxx'
     table = dynamodb.Table('customers')
 
-   # Create the DynamoDB table.
-    table.put_item(Item={
-            'username': username,
-            'name': name
-        }
-    )     
+    # Set customer's registration date
+    request.json['date'] = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+
+   # Create record in table
+    table.put_item(Item=request.json)     
 
     return 'Customer saved'
 
